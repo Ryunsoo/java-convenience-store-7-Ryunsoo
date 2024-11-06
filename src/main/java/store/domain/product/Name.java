a8e61ddb953a03b6e1ff1c3d76444acba45f44ae
babyfox225 <babyfox225@naver.com>
2024-11-07 00:41:22 +0900

    feat(name): 이름 생성 시 null & empty 예외 처리 추가

--- src/main/java/store/domain/product/Name.java
+++ src/main/java/store/domain/product/Name.java
@@ -5,7 +5,14 @@ public class Name {
     private final String name;
 
     public Name(String name) {
+        validate(name);
         this.name = name;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("상품명이 올바르지 않습니다.");
+        }
     }
 
 }
--- src/test/java/store/domain/product/NameTest.java
+++ src/test/java/store/domain/product/NameTest.java
@@ -2,8 +2,11 @@ package store.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NameTest {
 
@@ -15,6 +18,15 @@ class NameTest {
         Name name = new Name(string);
 
         assertThat(name).extracting("name").isEqualTo(string);
+    }
+
+    @DisplayName("이름 생성 시 null 또는 빈문자열은 예외를 던진다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void nameCannotHasNullOrEmptyString(String string) {
+        assertThatThrownBy(() -> new Name(string))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품명이 올바르지 않습니다.");
     }
 
 }
