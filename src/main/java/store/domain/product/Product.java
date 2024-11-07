9a56d6444f519fa956a190a9ce2ea5b7351b81b1
babyfox225 <babyfox225@naver.com>
2024-11-07 22:04:56 +0900

    feat(product): 상품 생성 시 이름 또는 가격 null 예외 처리 추가

--- src/main/java/store/domain/product/Product.java
+++ src/main/java/store/domain/product/Product.java
@@ -6,8 +6,16 @@ public class Product {
     private final Price price;
 
     public Product(ProductName name, Price price) {
+        validate(name);
+        validate(price);
         this.name = name;
         this.price = price;
+    }
+
+    private void validate(Object object) {
+        if (object == null) {
+            throw new IllegalArgumentException("상품 생성에 실패했습니다.");
+        }
     }
 
 }
--- src/test/java/store/domain/product/ProductTest.java
+++ src/test/java/store/domain/product/ProductTest.java
@@ -2,8 +2,14 @@ package store.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ProductTest {
 
@@ -16,6 +22,23 @@ class ProductTest {
         Product product = new Product(name, price);
         assertThat(product).extracting("name").isEqualTo(name);
         assertThat(product).extracting("price").isEqualTo(price);
+    }
+
+    @DisplayName("상품명 또는 가격이 null 이면 예외를 던진다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidNameOrPrice")
+    void productShouldHaveNameAndPrice(ProductName name, Price price) {
+        assertThatThrownBy(() -> new Product(name, price))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("상품 생성에 실패했습니다.");
+    }
+
+    private static Stream<Arguments> provideInvalidNameOrPrice() {
+        return Stream.of(
+                Arguments.of(null, Price.valueOf(1000)),
+                Arguments.of(new ProductName("이름"), null),
+                Arguments.of(null, null)
+        );
     }
 
 }
