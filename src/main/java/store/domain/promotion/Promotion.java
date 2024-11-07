65545a390e9952f932aea9e5fabe7c7425b0c7de
babyfox225 <babyfox225@naver.com>
2024-11-08 00:26:35 +0900

    feat(promotion): 프로모션 생성 시 null 예외 처리 추가

--- src/main/java/store/domain/promotion/Promotion.java
+++ src/main/java/store/domain/promotion/Promotion.java
@@ -9,9 +9,22 @@ public class Promotion {
     private final Period period;
 
     public Promotion(Name name, Benefit benefit, Period period) {
+        validate(name, benefit, period);
         this.name = name;
         this.benefit = benefit;
         this.period = period;
+    }
+
+    private void validate(Name name, Benefit benefit, Period period) {
+        validate(name);
+        validate(benefit);
+        validate(period);
+    }
+
+    private void validate(Object object) {
+        if (object == null) {
+            throw new IllegalArgumentException("프로모션 생성에 실패했습니다.");
+        }
     }
 
 }
--- src/test/java/store/domain/promotion/PromotionTest.java
+++ src/test/java/store/domain/promotion/PromotionTest.java
@@ -1,12 +1,18 @@
 package store.domain.promotion;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import store.domain.common.Name;
 
 import java.time.LocalDate;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PromotionTest {
 
@@ -21,6 +27,25 @@ class PromotionTest {
 
         assertThat(promotion).extracting("name", "benefit", "period")
                 .containsExactly(name, benefit, period);
+    }
+
+    @DisplayName("이름, 혜택 또는 적용 기간이 null 이면 예외를 던진다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidElements")
+    void promotionCannotHaveAnyNull(Name name, Benefit benefit, Period period) {
+        assertThatThrownBy(() -> new Promotion(name, benefit, period))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("프로모션 생성에 실패했습니다.");
+    }
+
+    private static Stream<Arguments> provideInvalidElements() {
+        return Stream.of(
+                Arguments.of(null, new Benefit(1),
+                        Period.between(LocalDate.now(), LocalDate.now().plusDays(10))),
+                Arguments.of(new Name("반짝할인"), null,
+                        Period.between(LocalDate.now(), LocalDate.now().plusDays(10))),
+                Arguments.of(new Name("반짝할인"), new Benefit(1), null)
+        );
     }
 
 }
