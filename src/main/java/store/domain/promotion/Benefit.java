66ca0e67231bdcb7da4d6e8e954f66087346f9a1
babyfox225 <babyfox225@naver.com>
2024-11-07 23:44:51 +0900

    feat(benefit): 프로모션 혜택 적용 수량 1 미만 예외 처리 추가

--- src/main/java/store/domain/promotion/Benefit.java
+++ src/main/java/store/domain/promotion/Benefit.java
@@ -2,10 +2,19 @@ package store.domain.promotion;
 
 public class Benefit {
 
+    private static final int MIN_APPLY_QUANTITY = 1;
+
     private final int applyQuantity;
 
     public Benefit(int applyQuantity) {
+        validate(applyQuantity);
         this.applyQuantity = applyQuantity;
+    }
+
+    private void validate(int applyQuantity) {
+        if (applyQuantity < MIN_APPLY_QUANTITY) {
+            throw new IllegalArgumentException("혜택 적용 수량이 올바르지 않습니다.");
+        }
     }
 
 }
--- src/test/java/store/domain/promotion/BenefitTest.java
+++ src/test/java/store/domain/promotion/BenefitTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BenefitTest {
 
@@ -13,6 +14,14 @@ class BenefitTest {
         Benefit benefit = new Benefit(2);
 
         assertThat(benefit).extracting("applyQuantity").isEqualTo(2);
+    }
+
+    @DisplayName("혜택 적용 구매 수량이 1 미만이면 예외를 던진다.")
+    @Test
+    void BenefitCannotHaveApplyQuantityBelowOne() {
+        assertThatThrownBy(() -> new Benefit(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("혜택 적용 수량이 올바르지 않습니다.");
     }
 
 }
