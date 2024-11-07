bbbbef7ff4eadffc4b20b4773136072dd9133b6c
babyfox225 <babyfox225@naver.com>
2024-11-07 21:51:25 +0900

    feat(price): 가격 동등 비교 로직 추가

--- src/main/java/store/domain/product/Price.java
+++ src/main/java/store/domain/product/Price.java
@@ -1,6 +1,7 @@
 package store.domain.product;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Price {
 
@@ -24,4 +25,16 @@ public class Price {
         }
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Price price1 = (Price) o;
+        return Objects.equals(price, price1.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(price);
+    }
 }
--- src/test/java/store/domain/product/PriceTest.java
+++ src/test/java/store/domain/product/PriceTest.java
@@ -2,6 +2,8 @@ package store.domain.product;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.math.BigDecimal;
 
@@ -25,6 +27,16 @@ class PriceTest {
         assertThatThrownBy(() -> Price.valueOf(-1))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("가격이 올바르지 않습니다.");
+    }
+
+    @DisplayName("값이 값으면 같은 가격, 다르면 다른 가격이다.")
+    @ParameterizedTest
+    @CsvSource(value = {"1000, 1000, true", "1000, 100, false"})
+    void sameOrDifferentByValue(long value1, long value2, boolean expected) {
+        Price price1 = Price.valueOf(value1);
+        Price price2 = Price.valueOf(value2);
+
+        assertThat(price1.equals(price2)).isEqualTo(expected);
     }
 
 }
