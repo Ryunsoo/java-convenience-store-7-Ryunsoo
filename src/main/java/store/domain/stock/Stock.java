22eafd03579f6027b25687f195c3de6631651264
babyfox225 <babyfox225@naver.com>
2024-11-07 23:28:12 +0900

    feat(stock): 0 미만 재고 예외 처리 추가

--- src/main/java/store/domain/stock/Stock.java
+++ src/main/java/store/domain/stock/Stock.java
@@ -2,10 +2,19 @@ package store.domain.stock;
 
 public class Stock {
 
+    private static final int MIN_QUANTITY = 0;
+
     private final int quantity;
 
     public Stock(int quantity) {
+        validate(quantity);
         this.quantity = quantity;
+    }
+
+    private void validate(int quantity) {
+        if (quantity < MIN_QUANTITY) {
+            throw new IllegalArgumentException("재고는 0 이상이어야 합니다.");
+        }
     }
 
 }
--- src/test/java/store/domain/stock/StockTest.java
+++ src/test/java/store/domain/stock/StockTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StockTest {
 
@@ -13,6 +14,14 @@ class StockTest {
         Stock stock = new Stock(10);
 
         assertThat(stock).extracting("quantity").isEqualTo(10);
+    }
+
+    @DisplayName("수량이 0 미만인 경우 예외를 던진다.")
+    @Test
+    void stockCannotHaveQuantityBelowZero() {
+        assertThatThrownBy(() -> new Stock(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("재고는 0 이상이어야 합니다.");
     }
 
 }
