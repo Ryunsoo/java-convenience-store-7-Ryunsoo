b776655a507160185851c882e2354cfdcc6b32ea
babyfox225 <babyfox225@naver.com>
2024-11-11 22:33:39 +0900

    test(productStock): 재고 차감 테스트 작성

--- src/test/java/store/domain/stock/ProductStocksTest.java
+++ src/test/java/store/domain/stock/ProductStocksTest.java
@@ -2,6 +2,7 @@ package store.domain.stock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import store.domain.common.Name;
@@ -10,19 +11,23 @@ import store.domain.promotion.Period;
 import store.domain.promotion.Promotion;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ProductStocksTest {
 
+    private Name defaultPromotionName;
+    private Benefit defaultBenefit;
+    private Period defaultPeriod;
     private Promotion defaultPromotion;
 
     @BeforeEach
     void setup() {
-        Name name = new Name("프로모션");
-        Benefit benefit = new Benefit(2);
-        Period period = Period.between(LocalDate.now(), LocalDate.now().plusDays(5));
-        this.defaultPromotion = new Promotion(name, benefit, period);
+        this.defaultPromotionName = new Name("프로모션");
+        this.defaultBenefit = new Benefit(2);
+        this.defaultPeriod = Period.between(LocalDate.now(), LocalDate.now().plusDays(5));
+        this.defaultPromotion = new Promotion(defaultPromotionName, defaultBenefit, defaultPeriod);
     }
 
     @DisplayName("일반 재고와 프로모션 재고가 충분한지 확인한다.")
@@ -34,6 +39,28 @@ class ProductStocksTest {
         ProductStocks productStocks = new ProductStocks(basicStock, promotionStock);
 
         assertThat(productStocks.hasEnough(quantity)).isEqualTo(expected);
+    }
+
+    @DisplayName("프로모션 기간이면 프로모션 재고를 먼저 차감하고, 아니면 일반 재고를 먼저 차감한다.")
+    @ParameterizedTest
+    @CsvSource(value = {"2024-11-01, 4, 0", "2024-12-01, 0, 4"})
+    void deductStocksInPromotionPeriod(String purchaseDate, int remainBasicStock, int remainPromotionStock) {
+        Period period = Period.between(LocalDate.of(2024, 11, 1),
+                LocalDate.of(2024, 11, 30));
+
+        BasicStock basicStock = new BasicStock(new Stock(5));
+        PromotionStock promotionStock = new PromotionStock(new Stock(5), createPromotion(period));
+        ProductStocks productStocks = new ProductStocks(basicStock, promotionStock);
+
+        LocalDateTime dateTime = LocalDate.parse(purchaseDate).atStartOfDay();
+        productStocks.deduct(dateTime, 6);
+
+        assertThat(basicStock).extracting("stock").isEqualTo(new Stock(remainBasicStock));
+        assertThat(promotionStock).extracting("stock").isEqualTo(new Stock(remainPromotionStock));
+    }
+
+    private Promotion createPromotion(Period period) {
+        return new Promotion(defaultPromotionName, defaultBenefit, period);
     }
 
 }
