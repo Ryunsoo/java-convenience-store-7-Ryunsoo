ec641648920e9e67a8615d3d6bf953afb41911ac
babyfox225 <babyfox225@naver.com>
2024-11-11 16:43:35 +0900

    test(promotionResult): 주문서 생성, 반환 로직 테스트

--- src/test/java/store/domain/promotion/PromotionResultTest.java
+++ src/test/java/store/domain/promotion/PromotionResultTest.java
@@ -1,12 +1,24 @@
 package store.domain.promotion;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import store.domain.common.Name;
+import store.domain.order.OrderSheet;
+import store.domain.product.Price;
+import store.domain.product.Product;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static store.domain.common.OrderStatus.DONE;
 
 class PromotionResultTest {
+
+    private Product defaultProduct;
+
+    @BeforeEach
+    void setUp() {
+        this.defaultProduct = new Product(new Name("상품"), Price.valueOf(1000));
+    }
 
     @DisplayName("프로모션이 미적용 시 상태는 DONE, 혜택 수량은 모두 0이다.")
     @Test
@@ -57,6 +69,51 @@ class PromotionResultTest {
 
         assertThat(promotionResult).extracting("status").isEqualTo(DONE);
         assertThat(promotionResult.getGeneralQuantity()).isEqualTo(generalQuantity);
+    }
+
+    @DisplayName("프로모션 결과를 통해 주문서를 생성, 반환한다.")
+    @Test
+    void returnOrderSheet() {
+        BenefitResult benefitResult = new BenefitResult(4, 2);
+        int generalQuantity = 1;
+
+        PromotionResult promotionResult = PromotionResult.withPromotion(benefitResult, generalQuantity);
+
+        OrderSheet orderSheet = promotionResult.getOrderSheet(defaultProduct);
+        assertThat(orderSheet.getProduct()).isEqualTo(defaultProduct);
+        assertThat(orderSheet).extracting("benefitQuantity").isEqualTo(4);
+        assertThat(orderSheet).extracting("freeQuantity").isEqualTo(2);
+        assertThat(orderSheet).extracting("generalQuantity").isEqualTo(1);
+    }
+
+    @DisplayName("증정 수량을 추가하여 주문서를 생성, 반환한다.")
+    @Test
+    void returnOrderSheetWithOneMore() {
+        BenefitResult benefitResult = new BenefitResult(4, 2);
+        int generalQuantity = 2;
+
+        PromotionResult promotionResult = PromotionResult.morePromotion(benefitResult, generalQuantity);
+
+        OrderSheet orderSheet = promotionResult.getOrderSheetWithOneMore(defaultProduct);
+        assertThat(orderSheet.getProduct()).isEqualTo(defaultProduct);
+        assertThat(orderSheet).extracting("benefitQuantity").isEqualTo(6);
+        assertThat(orderSheet).extracting("freeQuantity").isEqualTo(3);
+        assertThat(orderSheet).extracting("generalQuantity").isEqualTo(0);
+    }
+
+    @DisplayName("일반 결제 수량은 제외하여 주문서를 생성, 반환한다.")
+    @Test
+    void returnOrderSheetWithoutGeneralQuantity() {
+        BenefitResult benefitResult = new BenefitResult(4, 2);
+        int generalQuantity = 2;
+
+        PromotionResult promotionResult = PromotionResult.withPromotion(benefitResult, generalQuantity);
+
+        OrderSheet orderSheet = promotionResult.getOrderSheetOnlyBenefit(defaultProduct);
+        assertThat(orderSheet.getProduct()).isEqualTo(defaultProduct);
+        assertThat(orderSheet).extracting("benefitQuantity").isEqualTo(4);
+        assertThat(orderSheet).extracting("freeQuantity").isEqualTo(2);
+        assertThat(orderSheet).extracting("generalQuantity").isEqualTo(0);
     }
 
 }
